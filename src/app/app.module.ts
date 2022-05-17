57e3df36d8e54239740f322d146d5e3e1357970d
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-17 16:30:31 -0400

    Layout básico de menú lateral de configuración

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ConfigComponent } from './config/config.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
